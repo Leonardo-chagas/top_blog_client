54a6f2727dc4ba588b453a118ebdbc354b1c9b84
Leonardo-chagas <leoschagas@hotmail.com>
2026-08-16 23:57:49 -0300

    criou a estrutura básica para a página de seleção de posts, utilizando os posts da categoria selecionada

--- src/components/PostSelection.tsx
+++ src/components/PostSelection.tsx
@@ -1,9 +1,38 @@
-
+import { useParams } from "react-router";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import type { PostData } from "../types";
 
 function PostSelection(){
+    const [posts, setPosts] = useState<PostData[]>([]);
+    const {id} = useParams();
+    const apiUrl = import.meta.env.VITE_API_URL
+
+    useEffect(() => {
+        axios.get(apiUrl + 'posts/' + id).then(
+            response => {
+                const apiInfo = response.data;
+                const parsedData = JSON.parse(apiInfo);
+                let data : PostData[] = []
+
+                parsedData.forEach((post:PostData) => {
+                    data.push({id: post.id, title: post.title, text: post.text});
+                })
+                setPosts(data);
+            }
+        ).catch(error => {
+            console.log(error)
+        })
+    }, [])
+
     return(
-        <>
-        </>
+        <div>
+            {
+            posts.map((post, index) => (
+                <h2 key={index}>{post.title}</h2>
+            ))
+            }
+        </div>
     );
 }
 
